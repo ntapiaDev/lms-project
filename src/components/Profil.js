be83104e8a8b72a730a0cc41730083f8345231b3
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-04-25 13:31:37 +0200

    Page de profil

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -1,26 +1,80 @@
 import { useState, useEffect } from "react";
-import { axiosPrivate } from "../api/axios";
+import axios, { axiosPrivate } from "../api/axios";
 import useAuth from "../hooks/useAuth";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{2,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 const Profil = () => {
   const [userFirstname, setUserFirstname ] = useState('');
+  const [validFirstname, setValidFirstname ] = useState(false);
+
   const [userLastname, setUserLastname ] = useState('');
+  const [validLastname, setValidLastname ] = useState(false);
+
   const [userDisplayname, setUserDisplayname ] = useState('');
+  const [validDisplayname, setValidDisplayname ] = useState(false);
+
   const [userRole, setUserRole ] = useState('');
+
   const [userEmail, setUserEmail ] = useState('');
-  // Reset pwd avec matchPwd
+  const [validUserEmail, setValidvalidUserEmail ] = useState(false);
+  
+  const [oldPassword, setOldPassword ] = useState('');
+  const [validOldPwd, setValidOldPwd] = useState(false);
+  const [newPassword, setNewPassword ] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [matchPassword, setMatchPassword ] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+
+  const [errMsg, setErrMsg] = useState('');
+  const [succMsg, setSuccMsg] = useState('');
 
   const { auth } = useAuth();
   console.log('coucou');
 
   useEffect(() => {
+    const result = USER_REGEX.test(userFirstname);
+    setValidFirstname(result);
+  }, [userFirstname])
 
+  useEffect(() => {
+    const result = USER_REGEX.test(userLastname);
+    setValidLastname(result);
+  }, [userLastname])
+
+  useEffect(() => {
+    const result = USER_REGEX.test(userDisplayname);
+    setValidDisplayname(result);
+  }, [userDisplayname])
+
+  useEffect(() => {
+    const result = EMAIL_REGEX.test(userEmail);
+    setValidvalidUserEmail(result);
+  }, [userEmail])
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(oldPassword);
+    setValidOldPwd(result);
+  }, [oldPassword])
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(newPassword);
+    setValidPwd(result);
+    const match = newPassword === matchPassword;
+    setValidMatch(match);
+  }, [newPassword, matchPassword])
+
+  useEffect(() => {
     const getProfil = async () => {
       try {
         const getData = await axiosPrivate.get(`wp/v2/users/${auth.id}?context=edit`);
         setUserFirstname(getData.data.first_name);
         setUserLastname(getData.data.last_name);
-        setUserDisplayname(getData.data.name);
+        setUserDisplayname(getData.data.nickname);
         if (getData.data.roles[0] === 'subscriber') {
           setUserRole('Élève')
         } else if (getData.data.roles[0] === 'editor') {
@@ -36,23 +90,74 @@ const Profil = () => {
         console.error(err);
       }
     }
-
     getProfil();
   }, [auth.id]);
 
   const handleInfoSubmit = async (e) => {
     e.preventDefault();
+    const v1 = userFirstname !== '' ? USER_REGEX.test(userFirstname) : true;
+    const v2 = userLastname !== '' ? USER_REGEX.test(userLastname) : true;
+    const v3 = userDisplayname !== '' ? USER_REGEX.test(userDisplayname) : true;
+    const v4 = EMAIL_REGEX.test(userEmail);
+    if (!v1 || !v2 || !v3 || !v4 ) {
+      setErrMsg("Entrées invalides");
+      setSuccMsg('');
+      return;
+    }
+    
     try {
-      const response = await axiosPrivate.post('wp/v2/users',
+      const response = await axiosPrivate.post(`wp/v2/users/${auth.id}`,
         {
           first_name: userFirstname,
           last_name: userLastname,
-          user_display_name: userDisplayname,
+          nickname: userDisplayname,
           email: userEmail
         });
       console.log(response);
+      setErrMsg('');
+      setSuccMsg("Informations mises à jour");
     } catch (err) {
       console.log(err)
+    }
+  }
+
+  const handlePwdSubmit = async (e) => {
+    e.preventDefault();
+    const v1 = PWD_REGEX.test(oldPassword);
+    const v2 = PWD_REGEX.test(newPassword);
+    const v3 = PWD_REGEX.test(matchPassword);
+    if (!v1 || !v2 || !v3 ) {
+      setErrMsg("Entrées invalides");
+      setSuccMsg('');
+      return;
+    }
+    try {
+      // Check de l'ancien mot de passe :
+      const checkPwd = await axios.post(`jwt-auth/v1/token`,
+        {
+            username: auth.user,
+            password: oldPassword,
+        },
+        {   
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+      // Mise à jour du nouveau mot de passe :
+      const response = await axiosPrivate.post(`wp/v2/users/${auth.id}`,
+        {
+          password: newPassword
+        });
+      console.log(response);
+      setErrMsg('')
+      setSuccMsg("Mot de passe mis à jour");
+    } catch (err) {
+      if (err.response.status === 403) {
+        setErrMsg("Mot de passe incorrect");
+        setSuccMsg('')
+      } else {
+        setErrMsg("Échec de la mise à jour");
+      }
     }
   }
 
@@ -62,6 +167,8 @@ const Profil = () => {
 
       <section className="infos">
         <h4>Informations personnelles :</h4>
+        <p className={errMsg? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <p className={succMsg? "succmsg" : "offscreen"} aria-live="assertive">{succMsg}</p>
         <form onSubmit={handleInfoSubmit}>
           <div className="line">
             <label htmlFor="user">
@@ -77,6 +184,8 @@ const Profil = () => {
           <div className="line">
             <label htmlFor="firstname">
               Prénom :
+              <FontAwesomeIcon icon={faCheck} className={validFirstname ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validFirstname || !userFirstname ? "hide" : "invalid"} />
             </label>
             <input 
               type="text" 
@@ -88,6 +197,8 @@ const Profil = () => {
           <div className="line">
             <label htmlFor="lastname">
               Nom :
+              <FontAwesomeIcon icon={faCheck} className={validLastname ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validLastname || !userLastname ? "hide" : "invalid"} />
             </label>
             <input 
               type="text" 
@@ -98,7 +209,9 @@ const Profil = () => {
           </div>
           <div className="line">
             <label htmlFor="displayname">
-              Nom à afficher :
+            Pseudonyme :
+            <FontAwesomeIcon icon={faCheck} className={validDisplayname ? "valid" : "hide"} />
+            <FontAwesomeIcon icon={faTimes} className={validDisplayname || !userDisplayname ? "hide" : "invalid"} />
             </label>
             <input 
               type="text" 
@@ -121,6 +234,8 @@ const Profil = () => {
           <div className="line">
             <label htmlFor="email">
               Adresse email :
+              <FontAwesomeIcon icon={faCheck} className={validUserEmail ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validUserEmail || !userEmail ? "hide" : "invalid"} />
             </label>
             <input 
               type="text" 
@@ -133,30 +248,45 @@ const Profil = () => {
         </form>
 
         <h4>Changer mon mot de passe :</h4>
-        <form>
+        <form onSubmit={handlePwdSubmit}>
           <div className="line">
             <label htmlFor="password">
               Mot de passe actuel
+              <FontAwesomeIcon icon={faCheck} className={validOldPwd ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validOldPwd || !oldPassword ? "hide" : "invalid"} />
             </label>
             <input
               type="password"
-              id="password" />
+              id="password"
+              onChange={(e) => setOldPassword(e.target.value)}
+              value={oldPassword}
+              required />
           </div>
           <div className="line">
           <label htmlFor="newPwd">
               Nouveau mot de passe
+              <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validPwd || !newPassword ? "hide" : "invalid"} />
             </label>
             <input
               type="password"
-              id="newPwd" />
+              id="newPwd"
+              onChange={(e) => setNewPassword(e.target.value)}
+              value={newPassword}
+              required />
           </div>
           <div className="line">
           <label htmlFor="matchPwd">
               Confirmation
+              <FontAwesomeIcon icon={faCheck} className={validMatch && matchPassword ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPassword ? "hide" : "invalid"} />
             </label>
             <input
               type="password"
-              id="matchPwd" />
+              id="matchPwd"
+              onChange={(e) => setMatchPassword(e.target.value)}
+              value={matchPassword}
+              required />
           </div>
           <button className="form-btn">Changer mon mot de passe</button>
         </form>
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { axiosPrivate } from "../api/axios";
 import { Link, useNavigate } from "react-router-dom";
 
-const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const USER_REGEX = /^[A-z][A-z0-9-_]{2,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
