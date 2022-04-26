8b597b73091add4e4ce4b8a6a6a5d2404e45272d
Nicolas <101086567+ntapiaDev@users.noreply.github.com>
2022-04-26 10:46:56 +0200

    Update accueil.js
    fix

--- src/components/accueil.js
+++ src/components/accueil.js
@@ -1,20 +1,19 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import useAuth from "../hooks/useAuth";
 
 import Cours from "./cours";
 
-
-
 function Accueil() {
+
+    const { auth } = useAuth();
 
         return (
             <div>
-                <h1>Bienvenue sur LMS Projet</h1> 
+                <h1>Bienvenue sur LMS Projet</h1>
+                <p>{auth?.user ? `Bonjour ${auth?.displayname}, vous êtes connecté.` : ''}</p>
                 <Cours />
             </div>
-            
         );
 }
-
 
 export default Accueil;
